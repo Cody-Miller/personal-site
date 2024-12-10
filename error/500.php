85f616fbb49a0bcca2cd9c873a39399106c91d83
Cody Miller <cody.miller102@gmail.com>
2024-12-10 17:03:28 -0500

    Initial Commit
    Getting started on making a personal website! For now I'm just adding
    this as a resume kind-of webpage. But I also want to talk more about my
    projects. I would also like to add more PHP related featues. Maybe a
    quote gen via API access, maybe blog stuff. Time will tell!

---
+++ error/500.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cody Miller - Error</title>
+    <link rel="icon" type="image/png" href="/favicon.ico">
+    <link rel="stylesheet" href="../css/main.css" type="text/css">
+</head>
+<body>
+<div class="toggle-container">
+    <input type="checkbox" id="darkModeToggle" class="toggle-checkbox">
+    <label for="darkModeToggle" class="toggle-label">
+        <span class="toggle-icon toggle-icon-sun">☀️</span>
+        <span class="toggle-icon toggle-icon-moon">🌙</span>
+        <span class="toggle-switch"></span>
+    </label>
+</div>
+<div class="container">
+    <header>
+        <h1>Error 500</h1>
+        <p class="title">An error was encountered, something went very wrong. Sorry about that!</p>
+        <a href="../">"Take me back please!"</a>
+    </header>
+    <footer>
+        <p>&copy; <?= date("Y"); ?> Cody Miller. All rights reserved.</p>
+    </footer>
+</div>
+<script src="../js/main.js"></script>
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cody Miller - Projects</title>
+    <link rel="icon" type="image/png" href="/favicon.ico">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+<body>
+<div class="toggle-container">
+    <input type="checkbox" id="darkModeToggle" class="toggle-checkbox">
+    <label for="darkModeToggle" class="toggle-label">
+        <span class="toggle-icon toggle-icon-sun">☀️</span>
+        <span class="toggle-icon toggle-icon-moon">🌙</span>
+        <span class="toggle-switch"></span>
+    </label>
+</div>
+<div class="container">
+    <header>
+        <h1>Cody Miller</h1>
+        <p class="title">Web Developer</p>
+        <p>Email: <a href="mailto:cody.miller102+site@gmail.com">cody.miller102@gmail.com</a></p>
+        <p>GitHub: <a href="https://github.com/Cody-Miller">github.com/Cody-Miller</a></p>
+    </header>
+    <section id="about">
+        <h2>Projects</h2>
+        <h3>SKS - Warehouse Management &amp; Logistics</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores aspernatur dolores eaque error
+            inventore quibusdam quos totam ut! Aperiam commodi delectus doloribus harum laboriosam odio possimus sit.
+            Commodi, enim.</p>
+        <h3>CapitalLan - Badge Site</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores aspernatur dolores eaque error
+            inventore quibusdam quos totam ut! Aperiam commodi delectus doloribus harum laboriosam odio possimus sit.
+            Commodi, enim.</p>
+        <h3>Chore Tracking App</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores aspernatur dolores eaque error
+            inventore quibusdam quos totam ut! Aperiam commodi delectus doloribus harum laboriosam odio possimus sit.
+            Commodi, enim.</p>
+        <h3>Discord Bot</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores aspernatur dolores eaque error
+            inventore quibusdam quos totam ut! Aperiam commodi delectus doloribus harum laboriosam odio possimus sit.
+            Commodi, enim.</p>
+        <a href="./">"Back to the overview please!"</a>
+    </section>
+    <footer>
+        <p>&copy; <?= date("Y"); ?> Cody Miller. All rights reserved.</p>
+    </footer>
+</div>
+<script src="js/main.js"></script>
+</body>
+</html>
